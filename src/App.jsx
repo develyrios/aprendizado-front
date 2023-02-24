9ebcdd351a338d78834bb1f2b907eb5ace41544f
Bev Feitosa <bevely1407@gmail.com>
2023-02-24 01:24:14 -0300

    <feat>:initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Card } from './components/Card'
+import { Header } from './components/Header'
+import GlobalStyle from './styles/GlobalStyle'
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Card />
+    </>
+  )
+}

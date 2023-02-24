10e915c0021943bd0d9244b3763692c6e16c6fec
Bev Feitosa <bevely1407@gmail.com>
2023-02-24 03:08:08 -0300

    <feat>:adiciona o exercício 3

---
+++ src/components/Exercicios/exercicio03.jsx
@@ -0,0 +1,16 @@
+import { Paragraph, Span } from "../../styles/Text"
+import { Card } from "../Card"
+
+
+export const Exercicio03 = (props) => {
+    return(
+        <Card title="Exercício #3 - Renderização condicional simplificada">
+            <Paragraph>O número {props.numero} é</Paragraph>
+            
+            { props.numero % 2 === 0 ? 
+            <Span>Par</Span>
+            : <Span>Ímpar</Span>
+            }
+        </Card>
+    )
+}

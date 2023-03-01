14e6fdc8ebaaa200e51e28652b2ef51484af9982
Bev Feitosa <bevely1407@gmail.com>
2023-03-01 18:46:42 -0300

    <feat>:adiciona os exercícios 5 e 6 ao index

--- src/components/Exercicios/index.jsx
+++ src/components/Exercicios/index.jsx
@@ -2,11 +2,15 @@ import { Exercicio01 } from "./Exercicio01"
 import { Exercicio02 } from "./Exercicio02"
 import { Exercicio03 } from "./Exercicio03"
 import { Exercicio04 } from "./Exercicio04"
+import { Exercicio05 } from "./Exercicio05"
+import { Exercicio06 } from "./Exercicio06"
 import { Container } from "./style"
 
 export const Exercicios = () => {
     return(
         <Container>
+            <Exercicio06 />
+            <Exercicio05 />
             <Exercicio04 numero={13}/>
             <Exercicio03 numero={6}/>
             <Exercicio02 />

983f4221ef1ffbe4bdb196f592ffc62fdffe2ebe
Bev Feitosa <bevely1407@gmail.com>
2023-04-05 18:24:22 -0300

    <feat>:adiciona efeitos de hover e active no componente Button

--- src/styles/Button.js
+++ src/styles/Button.js
@@ -4,4 +4,13 @@ export const Button = styled.button`
     border: 0;
     border-radius: 8px;
     padding: 4px 8px;
+    transition: 250ms;
+
+    :hover {
+        transform: scale(1.1);
+    }
+    
+    :active {
+        transform: scale(0.75);
+    }
 `
